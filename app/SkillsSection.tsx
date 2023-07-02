feae59377db59067e3b72dd3e6b01d361a5d29d8
Aran Clary Deltac <bluefeet@gmail.com>
2023-07-02 07:22:27 -0700

    feat(theme): add light mode

--- app/SkillsSection.tsx
+++ app/SkillsSection.tsx
@@ -6,10 +6,10 @@ const SkillsSection = ({ className = '' }: { className?: string }) => {
   if (!resume.profile?.skills?.length) return <></>
 
   return <>
-    <section className={className}>
+    <section className={`mb-4 ${className}`}>
       <SectionTitle>Skills</SectionTitle>
 
-      <div className='typography mb-6'>
+      <div className='typography'>
         <ul>{resume.profile.skills.map((skill) => <li key={skill.name}>
           {skill.name}
           <Details>{skill.competencies?.join(', ')}</Details>
