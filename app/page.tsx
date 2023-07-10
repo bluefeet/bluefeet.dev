5ddbdadf9e21fa631e698ece94c001121ce88354
Aran Clary Deltac <bluefeet@gmail.com>
2023-07-10 13:40:13 -0700

    Slight footer adjustment in print mode

--- app/page.tsx
+++ app/page.tsx
@@ -14,12 +14,12 @@ const Divider = ({ className = '' }: { className?: string }) =>
   <hr className={`w-full border-zinc-400 dark:border-sky-600 border-solid border-1 mt-2 mb-2 print:hidden ${className}`} />
 
 const Page = () => <>
-  <header className='grid grid-cols-[max-content_max-content] text-white bg-sky-700 dark:bg-zinc-800 place-content-center text-center sticky top-0 h-24 md:h-28 lg:h-32 shadow-lg print:hidden'>
+  <header className={`grid grid-cols-[max-content_max-content] text-white bg-sky-700 dark:bg-zinc-800 place-content-center text-center sticky top-0 h-24 md:h-28 lg:h-32 shadow-lg print:hidden ${headerFont.className}`}>
     <img src='/avatar.jpg' alt={`Photo of ${resume.contact?.fullName}`} className='row-span-2 h-20 md:h-24 lg:h-28 rounded-full border-solid border-4 border-sky-800 mr-6' />
-    <h1 className={`text-5xl lg:text-6xl self-end ${headerFont.className}`}>
+    <h1 className='text-5xl lg:text-6xl self-end'>
       {resume.contact?.fullName}
     </h1>
-    <p className={`self-start pt-1 ${headerFont.className}`}>
+    <p className='self-start pt-1'>
       {resume.profile?.headline} • {resume.contact?.pronouns}
     </p>
   </header>
@@ -61,10 +61,9 @@ const Page = () => <>
     </Details>
   </footer>
 
-  <footer className='text-right pt-4 hidden print:block'>
-    <Details>
-      &copy; {resume.contact?.fullName} • Generated {formatDate(new Date,'PPP')}
-    </Details>
+  <footer className='text-right pt-8 hidden print:flex justify-between'>
+    <Details>Generated {formatDate(new Date,'PPP')}</Details>
+    <Details>&copy; {resume.contact?.fullName}</Details>
   </footer>
 </>
 
