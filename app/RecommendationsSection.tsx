f0f6677e819f8dcda38036c5e3b165f6c2748530
Aran Clary Deltac <bluefeet@gmail.com>
2023-07-12 14:23:25 -0700

    refactor(ui): restyle recommendations

--- app/RecommendationsSection.tsx
+++ app/RecommendationsSection.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react'
 import { Recommendation } from '@/types/Resume'
 import Details from './Details'
 import ReactMarkdown from 'react-markdown'
@@ -6,14 +7,17 @@ import SectionTitle from './SectionTitle'
 import SubSectionTitle from './SubSectionTitle'
 
 const Recommendation = ({ recommendation }: { recommendation: Recommendation }) => <>
-    <SubSectionTitle>
-      {recommendation.author}
-    </SubSectionTitle>
+  <div className='flex pt-8'>
+    <div className={`text-6xl pr-4 text-sky-400`}>“</div>
     <div className='typography'>
-      <Details>{recommendation.relationship}</Details>
       <ReactMarkdown>{recommendation.message}</ReactMarkdown>
     </div>
-  </>
+  </div>
+  <div className='text-right pt-2 pb-8 pr-2'>
+    <p className='text-xl'>- {recommendation.author}</p>
+    <Details className='pl-16'>{recommendation.relationship}</Details>
+  </div>
+</>
 
 const RecommendationsSection = ({ className = '' }: { className?: string }) => {
   if (!resume.recommendations?.length) return <></>
@@ -21,6 +25,7 @@ const RecommendationsSection = ({ className = '' }: { className?: string }) => {
   return <>
     <section className={className}>
       <SectionTitle>Recommendations</SectionTitle>
+
       {resume.recommendations.map((recommendation) =>
         <Recommendation
           recommendation={recommendation}
