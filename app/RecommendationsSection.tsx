2279e0df4081114630cb9674167d775b4c3a4939
Aran Clary Deltac <bluefeet@gmail.com>
2023-07-14 15:14:55 -0700

    fix(layout): tighten recommendations layout

--- app/RecommendationsSection.tsx
+++ app/RecommendationsSection.tsx
@@ -1,21 +1,21 @@
-import { ReactNode } from 'react'
 import { Recommendation } from '@/types/Resume'
 import Details from './Details'
 import ReactMarkdown from 'react-markdown'
 import resume from './resume'
 import SectionTitle from './SectionTitle'
-import SubSectionTitle from './SubSectionTitle'
 
 const Recommendation = ({ recommendation }: { recommendation: Recommendation }) => <>
-  <div className='flex pt-8'>
-    <div className={`text-6xl pr-4 text-sky-400`}>“</div>
-    <div className='typography'>
-      <ReactMarkdown>{recommendation.message}</ReactMarkdown>
+  <div className='pt-8 pb-8'>
+    <div className='flex'>
+      <div className={`text-6xl pr-4 text-sky-400`}>“</div>
+      <div className='typography'>
+        <ReactMarkdown>{recommendation.message}</ReactMarkdown>
+      </div>
     </div>
-  </div>
-  <div className='text-right pt-2 pb-8 pr-2'>
-    <p className='text-xl'>- {recommendation.author}</p>
-    <Details className='pl-16'>{recommendation.relationship}</Details>
+    <div className='pl-16'>
+      <p className='text-xl'>{recommendation.author}</p>
+      <Details className=''>{recommendation.relationship}</Details>
+    </div>
   </div>
 </>
 
