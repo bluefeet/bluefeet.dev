6e336d5cb35c5883de9caca79f132f4b1318fa87
Aran Clary Deltac <bluefeet@gmail.com>
2023-08-09 13:41:46 -0700

    fix(links): preserve production link history
    Treat external links differently so production navigation behaves
    correctly without Next.js trying to manage history or prefetching for
    them.

--- app/Link.tsx
+++ app/Link.tsx
@@ -1,8 +1,23 @@
 import NextLink from 'next/link'
 import { ReactNode } from 'react'
 
-const Link = ({ href, className = '', children }: { href: string, className?: string, children: ReactNode }) =>
-  <NextLink href={href} className={`text-sky-400 underline decoration-sky-800 hover:decoration-sky-400 print:text-black print:no-underline ${className}`}>
+const Link = ({
+  href,
+  className = '',
+  children,
+  external = false,
+}: {
+  href: string,
+  className?: string,
+  children: ReactNode,
+  external?: boolean,
+}) =>
+  <NextLink
+    href={href}
+    prefetch={!external}
+    passHref={external}
+    className={`text-sky-400 underline decoration-sky-800 hover:decoration-sky-400 print:text-black print:no-underline ${className}`}
+  >
     {children}
   </NextLink>
 
