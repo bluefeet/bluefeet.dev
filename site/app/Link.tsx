eb614c26d10f47f30ba182b006515aa80e2acdbd
Aran Clary Deltac <bluefeet@gmail.com>
2023-09-04 15:20:39 -0700

    fix(links): use native anchor for external links

--- site/app/Link.tsx
+++ site/app/Link.tsx
@@ -1,24 +1,19 @@
-import NextLink from 'next/link'
 import { ReactNode } from 'react'
 
 const Link = ({
   href,
   className = '',
   children,
-  external = false,
 }: {
   href: string,
   className?: string,
   children: ReactNode,
-  external?: boolean,
 }) =>
-  <NextLink
+  <a
     href={href}
-    prefetch={!external}
-    passHref={external}
     className={`text-sky-400 underline decoration-sky-800 hover:decoration-sky-400 print:text-black print:no-underline ${className}`}
   >
     {children}
-  </NextLink>
+  </a>
 
 export default Link
