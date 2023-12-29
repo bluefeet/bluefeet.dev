daa00cc827c802615edbff662a2bb3f7d000e747
Aran Clary Deltac <bluefeet@gmail.com>
2023-12-28 22:52:49 -0800

    Make buttons fixed

--- app/page.tsx
+++ app/page.tsx
@@ -11,45 +11,66 @@ import ObjectiveSection from './ObjectiveSection'
 import RecommendationsSection from './RecommendationsSection'
 import resume from './resume'
 import SkillsSection from './SkillsSection'
-import { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Howl } from 'howler'
 
 import {
   ArrowDownIcon,
-  SpeakerWaveIcon,
-  SpeakerXMarkIcon,
-} from '@heroicons/react/24/solid'
+  PauseIcon,
+  PlayIcon,
+} from '@heroicons/react/24/outline'
 
 const Divider = ({ className = '' }: { className?: string }) =>
   <hr className={`w-full border-sky-600 border-solid border-1 mt-2 mb-2 print:hidden ${className}`} />
 
-const Page = () => {
-  const mainRef = useRef<HTMLElement>(null);
-  const [sound, setSound] = useState<Howl | null>(null);
-  const [muted, setMuted] = useState(true);
+const DashButton: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = (props) =>
+  <button {...props} className='p-[4px] border-[2px] border-solid border-amber-500 bg-zinc-800/50 focus:outline-none focus:p-[2px] focus:border-[4px]'>{props.children}</button>
 
-  const scrollToMain = () => {
-    mainRef.current?.scrollIntoView({ behavior: 'smooth' })
-  }
+const Page = () => {
+  const mainRef = useRef<HTMLElement>(null)
+  const [audio, setAudio] = useState<Howl | null>(null)
+  const [isPlaying, setPlaying] = useState(false)
+  const [isNearTop, setIsNearTop] = useState(true)
 
   useEffect(() => {
-    if (muted || sound) return
-    setSound(new Howl({
+    if (!isPlaying || audio) return
+    setAudio(new Howl({
+      html5: true, // Makes iOS Safari happier when phone is locked.
       src: ['/nature.mp3'],
       loop: true,
+      // Catch when external processes (the browser or OS) affect playing.
+      onplay: () => setPlaying(true),
+      onpause: () => setPlaying(false),
+      onstop: () => setPlaying(false),
     }))
-  }, [muted])
+  }, [isPlaying])
 
   useEffect(() => {
-    if (muted) sound?.pause()
-    else sound?.play()
-  }, [muted, sound])
+    if (!isPlaying) audio?.pause()
+    else audio?.play()
+  }, [isPlaying, audio])
 
-  const toggleMute = () => {
-    setMuted((wasMuted) => !wasMuted)
-  }
+  useEffect(() => {
+    if (audio && navigator && navigator.mediaSession) {
+      navigator.mediaSession.setActionHandler('play', () => audio.play())
+      navigator.mediaSession.setActionHandler('pause', () => audio.pause())
+    }
+  }, [audio])
 
-  const SoundIcon = muted ? SpeakerXMarkIcon : SpeakerWaveIcon
+  useEffect(() => {
+    const handleScroll = () => setIsNearTop(window.scrollY < window.innerHeight / 2)
+    handleScroll() // Call once in case the window is scrolled down.
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  const startPlaying = () => setPlaying(true)
+  const pausePlaying = () => setPlaying(false)
+
+  const scrollToMain = () => mainRef.current?.scrollIntoView({ behavior: 'smooth' })
+  const scrollToTop = () => window.scrollTo({ top: 0, behavior: 'smooth' })
+
+  const PlayPauseIcon = isPlaying ? PauseIcon : PlayIcon
 
   return <>
     <header className='bg-bear bg-cover h-screen bg-center print:hidden border-b-2 border-zinc-950 border-solid'>
@@ -62,14 +83,6 @@ const Page = () => {
           {resume.profile?.headline} • {resume.contact?.pronouns}
         </p>
       </div>
-      <div className='absolute bottom-4 right-4 w-max'>
-        <button onClick={toggleMute} className='border-2 border-amber-500 border-solid p-1 mr-4' aria-label='Toggle Background Sound'>
-          <SoundIcon className='w-10 h-10' />
-        </button>
-        <button onClick={scrollToMain} className='border-2 border-amber-500 border-solid p-1' aria-label='Scroll to Main Content'>
-          <ArrowDownIcon className='w-10 h-10' />
-        </button>
-      </div>
     </header>
 
     <header className={`pb-4 ${headerFont.className} hidden print:flex justify-between items-end`}>
@@ -80,6 +93,15 @@ const Page = () => {
         {resume.profile?.headline} • {resume.contact?.pronouns} • https://bluefeet.dev
       </p>
     </header>
+
+    <div className='fixed flex gap-x-4 bottom-4 right-4 w-max print:hidden'>
+      <DashButton onClick={isPlaying ? pausePlaying : startPlaying} aria-label={isPlaying ? 'Pause Nature Sounds' : 'Play Nature Sounds'}>
+        <PlayPauseIcon className='w-10 h-10' />
+      </DashButton>
+      <DashButton onClick={isNearTop ? scrollToMain : scrollToTop} aria-label={isNearTop ? 'Scroll to Content' : 'Scroll to Top'}>
+        <ArrowDownIcon className={`w-10 h-10 transition ease-in-out duration-300 ${isNearTop ? '' : 'rotate-180'}`} />
+      </DashButton>
+    </div>
 
     <main ref={mainRef} className='flex flex-col lg:flex-row p-4 md:p-8 pt-0 lg:pt-8 ml-auto mr-auto max-w-6xl print:p-0'>
       <div className='lg:w-2/5 flex flex-wrap lg:flex-col lg:order-2 lg:pl-8 print:flex-row print:pl-0 print:pb-2'>
@@ -103,7 +125,7 @@ const Page = () => {
       </div>
     </main>
 
-    <footer className='border-zinc-800 border-solid border-t-2 text-center p-4 print:hidden'>
+    <footer className='border-zinc-800 border-solid border-t-2 text-center p-4 pb-20 print:hidden'>
       <Details>
         Site built with <Link href='https://nextjs.org/'>Next.js</Link> and <Link href='https://tailwindcss.com/'>Tailwind CSS</Link>, hosted for free by <Link href='https://pages.cloudflare.com/'>Cloudflare</Link>, source on <Link href='https://github.com/bluefeet/bluefeet.dev'>GitHub</Link>.<br />
         &copy; {resume.contact?.fullName}
