c71efec446e83c8555658a83c053ba47f3464dd0
Aran Clary Deltac <bluefeet@gmail.com>
2023-12-29 15:20:23 -0800

    Make dash buttons sticky, refactor Page

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import ObjectiveSection from './ObjectiveSection'
 import RecommendationsSection from './RecommendationsSection'
 import resume from './resume'
 import SkillsSection from './SkillsSection'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { createContext, useContext, useEffect, useRef, useState } from 'react'
 import { Howl } from 'howler'
 
 import {
@@ -23,14 +23,15 @@ import {
 const Divider = ({ className = '' }: { className?: string }) =>
   <hr className={`w-full border-sky-600 border-solid border-1 mt-2 mb-2 print:hidden ${className}`} />
 
+// Used by Page to store a ref used by both MainContent and ScrollDashButton.
+const mainRefContext = createContext<React.RefObject<HTMLElement> | null>(null)
+
 const DashButton: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = (props) =>
   <button {...props} className='p-[4px] border-[2px] border-solid border-amber-500 bg-zinc-800/50 focus:outline-none focus:p-[2px] focus:border-[4px]'>{props.children}</button>
 
-const Page = () => {
-  const mainRef = useRef<HTMLElement>(null)
+const AudioDashButton = () => {
   const [audio, setAudio] = useState<Howl | null>(null)
   const [isPlaying, setPlaying] = useState(false)
-  const [isNearTop, setIsNearTop] = useState(true)
 
   useEffect(() => {
     if (!isPlaying || audio) return
@@ -57,6 +58,22 @@ const Page = () => {
     }
   }, [audio])
 
+  const startPlaying = () => setPlaying(true)
+  const pausePlaying = () => setPlaying(false)
+
+  const PlayPauseIcon = isPlaying ? PauseIcon : PlayIcon
+
+  return (
+    <DashButton onClick={isPlaying ? pausePlaying : startPlaying} aria-label={isPlaying ? 'Pause Nature Sounds' : 'Play Nature Sounds'}>
+      <PlayPauseIcon className='w-10 h-10' />
+    </DashButton>
+  )
+}
+
+const ScrollDashButton = () => {
+  const [isNearTop, setIsNearTop] = useState(true)
+  const mainRef = useContext(mainRefContext)
+
   useEffect(() => {
     const handleScroll = () => setIsNearTop(window.scrollY < window.innerHeight / 2)
     handleScroll() // Call once in case the window is scrolled down.
@@ -64,45 +81,49 @@ const Page = () => {
     return () => window.removeEventListener('scroll', handleScroll)
   }, [])
 
-  const startPlaying = () => setPlaying(true)
-  const pausePlaying = () => setPlaying(false)
+  const scrollToMain = () => mainRef?.current?.scrollIntoView({ behavior: 'smooth' })
+  const scrollToTop = () => { console.log('SCROLL TOP'); window.scrollTo({ top: 0, behavior: 'smooth' }) }
 
-  const scrollToMain = () => mainRef.current?.scrollIntoView({ behavior: 'smooth' })
-  const scrollToTop = () => window.scrollTo({ top: 0, behavior: 'smooth' })
+  return (
+    <DashButton onClick={isNearTop ? scrollToMain : scrollToTop} aria-label={isNearTop ? 'Scroll to Content' : 'Scroll to Top'}>
+      <ArrowDownIcon className={`w-10 h-10 transition ease-in-out duration-300 ${isNearTop ? '' : 'rotate-180'}`} />
+    </DashButton>
+  )
+}
 
-  const PlayPauseIcon = isPlaying ? PauseIcon : PlayIcon
+const DashButtons = () =>
+  <div className='sticky flex justify-end gap-x-4 bottom-0 p-4 print:hidden'>
+    <AudioDashButton />
+    <ScrollDashButton />
+  </div>
 
-  return <>
-    <header className='bg-bear bg-cover h-screen bg-center print:hidden border-b-2 border-zinc-950 border-solid'>
-      <div className='absolute h-48 w-full bg-gradient-to-b from-zinc-900 opacity-50' />
-      <div className='absolute w-full text-center md:text-right p-5 md:pr-16 lg:pr-24'>
-        <h1 className='text-6xl md:text-7xl lg:text-8xl md:pr-16 lg:pr-32'>
-          {resume.contact?.fullName}
-        </h1>
-        <p className='pt-2 md:pt-3 lg:pt-5 lg:pr-16 text-lg md:text-2xl'>
-          {resume.profile?.headline} • {resume.contact?.pronouns}
-        </p>
-      </div>
-    </header>
-
-    <header className={`pb-4 ${headerFont.className} hidden print:flex justify-between items-end`}>
-      <h1 className='text-5xl font-semibold text-sky-800'>
+const HeaderForDisplay = () =>
+  <header className='bg-bear bg-cover h-screen bg-center print:hidden border-b-2 border-zinc-950 border-solid'>
+    <div className='absolute h-48 w-full bg-gradient-to-b from-zinc-900 opacity-50' />
+    <div className='absolute w-full text-center md:text-right p-5 md:pr-16 lg:pr-24'>
+      <h1 className='text-6xl md:text-7xl lg:text-8xl md:pr-16 lg:pr-32'>
         {resume.contact?.fullName}
       </h1>
-      <p>
-        {resume.profile?.headline} • {resume.contact?.pronouns} • https://bluefeet.dev
+      <p className='pt-2 md:pt-3 lg:pt-5 lg:pr-16 text-lg md:text-2xl'>
+        {resume.profile?.headline} • {resume.contact?.pronouns}
       </p>
-    </header>
-
-    <div className='fixed flex gap-x-4 bottom-4 right-4 w-max print:hidden'>
-      <DashButton onClick={isPlaying ? pausePlaying : startPlaying} aria-label={isPlaying ? 'Pause Nature Sounds' : 'Play Nature Sounds'}>
-        <PlayPauseIcon className='w-10 h-10' />
-      </DashButton>
-      <DashButton onClick={isNearTop ? scrollToMain : scrollToTop} aria-label={isNearTop ? 'Scroll to Content' : 'Scroll to Top'}>
-        <ArrowDownIcon className={`w-10 h-10 transition ease-in-out duration-300 ${isNearTop ? '' : 'rotate-180'}`} />
-      </DashButton>
     </div>
+  </header>
 
+const HeaderForPrint = () =>
+  <header className={`pb-4 ${headerFont.className} hidden print:flex justify-between items-end`}>
+    <h1 className='text-5xl font-semibold text-sky-800'>
+      {resume.contact?.fullName}
+    </h1>
+    <p>
+      {resume.profile?.headline} • {resume.contact?.pronouns} • https://bluefeet.dev
+    </p>
+  </header>
+
+const MainContent = () => {
+  const mainRef = useContext(mainRefContext)
+
+  return (
     <main ref={mainRef} className='flex flex-col lg:flex-row p-4 md:p-8 pt-0 lg:pt-8 ml-auto mr-auto max-w-6xl print:p-0'>
       <div className='lg:w-2/5 flex flex-wrap lg:flex-col lg:order-2 lg:pl-8 print:flex-row print:pl-0 print:pb-2'>
         <AboutSection className='print:pb-2' />
@@ -124,19 +145,37 @@ const Page = () => {
         <RecommendationsSection className='lg:pt-4 print:hidden' />
       </div>
     </main>
+  )
+}
 
-    <footer className='border-zinc-800 border-solid border-t-2 text-center p-4 pb-20 print:hidden'>
-      <Details>
-        Site built with <Link href='https://nextjs.org/'>Next.js</Link> and <Link href='https://tailwindcss.com/'>Tailwind CSS</Link>, hosted for free by <Link href='https://pages.cloudflare.com/'>Cloudflare</Link>, source on <Link href='https://github.com/bluefeet/bluefeet.dev'>GitHub</Link>.<br />
-        &copy; {resume.contact?.fullName}
-      </Details>
-    </footer>
+const FooterForDisplay = () =>
+  <footer className='border-zinc-800 border-solid border-t-2 text-center p-4 print:hidden'>
+    <Details>
+      Site built with <Link href='https://nextjs.org/'>Next.js</Link> and <Link href='https://tailwindcss.com/'>Tailwind CSS</Link>, hosted for free by <Link href='https://pages.cloudflare.com/'>Cloudflare</Link>, source on <Link href='https://github.com/bluefeet/bluefeet.dev'>GitHub</Link>.<br />
+      &copy; {resume.contact?.fullName}
+    </Details>
+  </footer>
 
-    <footer className='text-right pt-8 hidden print:flex justify-between'>
-      <Details>Generated {formatDate(new Date, 'PPP')}</Details>
-      <Details>&copy; {resume.contact?.fullName}</Details>
-    </footer>
-  </>
+const FooterForPrint = () =>
+  <footer className='text-right pt-8 hidden print:flex justify-between'>
+    <Details>Generated {formatDate(new Date, 'PPP')}</Details>
+    <Details>&copy; {resume.contact?.fullName}</Details>
+  </footer>
+
+
+const Page = () => {
+  const mainRef = useRef<HTMLElement>(null)
+
+  return (
+    <mainRefContext.Provider value={mainRef}>
+      <HeaderForDisplay />
+      <HeaderForPrint />
+      <MainContent />
+      <DashButtons />
+      <FooterForDisplay />
+      <FooterForPrint />
+    </mainRefContext.Provider>
+  )
 }
 
 export default Page
