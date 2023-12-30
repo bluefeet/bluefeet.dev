c87e0a1056f492f7d9dbdc508b7eadaf2350b0c0
Aran Clary Deltac <bluefeet@gmail.com>
2023-12-30 11:18:36 -0800

    Add jest.setup.ts

--- app/Link.test.tsx
+++ app/Link.test.tsx
@@ -1,7 +1,6 @@
-import '@testing-library/jest-dom/jest-globals'
 import { render, screen } from '@testing-library/react'
+
 import Link from './Link'
-import { expect, describe, it } from '@jest/globals';
 
 describe('Link', () => {
   it('renders', () => {
