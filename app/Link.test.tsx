fb92e4a6d8b0943d76e58c54dc11a050f006870b
Aran Clary Deltac <bluefeet@gmail.com>
2023-12-30 10:02:23 -0800

    Make the Link test be useful

--- app/Link.test.tsx
+++ app/Link.test.tsx
@@ -3,12 +3,20 @@ import { render, screen } from '@testing-library/react'
 import Link from './Link'
 import { expect, describe, it } from '@jest/globals';
 
-describe('Home', () => {
-  it('renders a heading', () => {
+describe('Link', () => {
+  it('renders', () => {
     render(<Link href='foo.html'>Bar</Link>)
-
-    const link = screen.getByRole('link')
-
+    const link = screen.getByRole<HTMLLinkElement>('link')
     expect(link).toBeInTheDocument()
+    expect(link.href).toBe('http://localhost/foo.html')
+    expect(link.innerHTML).toBe('Bar')
+  })
+
+  it('merges custom and built-in classes', () => {
+    render(<Link href='foo.html' className='test-foo'>Bar</Link>)
+    const link = screen.getByRole('link')
+    expect(link.className).toMatch(/\btest-foo\b/) // Custom, from above
+    expect(link.className).toMatch(/\bunderline\b/) // Built-in, provided by Link
+    expect(link.className).not.toMatch(/\btest-does-not-exist\b/) // Negative test, just in case
   })
 })
